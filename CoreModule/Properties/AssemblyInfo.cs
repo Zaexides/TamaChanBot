f5a45878c99d1ebf9a7edab114c538aff10d77f5
Zaexides <JeroenEndenburg@hotmail.com>
2018-08-07 20:48:35 +0200

    Assembly Copyright info

--- CoreModule/Properties/AssemblyInfo.cs
+++ CoreModule/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CoreModule")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018 Zaexides")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- TamaChanBot/Properties/AssemblyInfo.cs
+++ TamaChanBot/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TamaChanBot")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Discord.NET bot for Discord")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TamaChanBot")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018 Zaexides")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
